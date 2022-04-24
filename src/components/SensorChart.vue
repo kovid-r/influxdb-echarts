b8595bfc60309cc8d986b89df26c560cd991ba21
Kovid Rathee <kovid.rathee@protonmail.com>
2022-04-24 12:07:19 +1000

    Change data arrow function to normal function

--- src/components/SensorChart.vue
+++ src/components/SensorChart.vue
@@ -2,7 +2,6 @@
   <div>
     <div class="chart-wrapper">
       <chart :options="chartOptionsLine">
-        {{sampleData}}
       </chart>
     </div>
   </div>
@@ -13,32 +12,34 @@
 export default {
   name: "SensorChart",
   props: ["sampleData","sampleLabels"],
-  data: () => ({
-    chartOptionsLine: {
-      xAxis: {
-        data: ["S","M","T",'W',"T","F","S"]
-      },
-      yAxis: {
-        type: "value"
-      },
 
-      series: [
-        {
-          type: "line",
-          // data: this.sampleData
-          data: [1, 4, 6, 7, 26, 36, 19]
-        }
-      ],
-      title: {
-        text: "Sensor Data",
-        x: "center",
-        textStyle: {
-          fontSize: 24
-        }
-      },
-      color: ["#127ac2"]
+  data: function() { 
+    return {
+      chartOptionsLine: { 
+        xAxis: {
+          data: this.sampleLabels
+        },
+        yAxis: {
+          type: "value"
+        },
+
+        series: [
+          {
+            type: "line",
+            data: this.sampleData
+          }
+        ],
+        title: {
+          text: "Sensor Data",
+          x: "center",
+          textStyle: {
+            fontSize: 24
+          }
+        },
+        color: ["#127ac2"]
+      }
     }
-  })
+  }
 };
 
 </script>
