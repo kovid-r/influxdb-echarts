6fe615acd67abff16cf1bfb61e391dd69d6149b9
Kovid Rathee <kovid.rathee@protonmail.com>
2022-04-25 04:13:42 +1000

    Fix database connection

--- src/main.js
+++ src/main.js
@@ -10,40 +10,36 @@ const token = 'v96hkhxEFt0zeXtfPk4heXIUOItCU6em867gAE00R1Gvq8LGE1KsBZNkHjMrejRL6
 const org = 'kovid.rathee@gmail.com'
 const url = 'https://us-east-1-1.aws.cloud2.influxdata.com'
 
-// const fluxQuery = `\
-//   sample.data(set: "airSensor")
-//   |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-//   |> filter(fn: (r) => r["_measurement"] == "airSensors")
-//   |> filter(fn: (r) => r["_field"] == "temperature")
-//   |> filter(fn: (r) => r["sensor_id"] == "TLM0100")`;
-
 const queryApi = new InfluxDB({url, token}).getQueryApi(org)
+// window.console.log(queryApi);
 
-const fluxQuery = `\
-//   sample.data(set: "airSensor")
-//   |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-//   |> filter(fn: (r) => r["_measurement"] == "airSensors")
-//   |> filter(fn: (r) => r["_field"] == "temperature")
-//   |> filter(fn: (r) => r["sensor_id"] == "TLM0100")`;
+const fluxQuery = `from(bucket: "kovid.rathee's Bucket")|> range(start: 0)`
 
 let sensorValues = [];
 
-/** Execute a query and receive line table metadata and rows. */
-// queryApi.queryRows(fluxQuery, fluxObserver)
-
-queryApi
-  .collectRows(fluxQuery /*, you can specify a row mapper as a second arg */)
-  .then(data => {
-    data.forEach(x => sensorValues.push(x))
-    window.console.log('\nCollect ROWS SUCCESS')
+queryApi.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+      // the following line creates an object for each row
+      const o = tableMeta.toObject(row)
+      // console.log(JSON.stringify(o, null, 2))
+      // window.console.log(
+      //   o
+      // )
+      sensorValues.push(o)
+    },
+    error: (error) => {
+      window.console.error(error)
+      window.console.log('\nFinished ERROR')
+    },
+    complete: () => {
+      window.console.log('\nFinished SUCCESS')
+    },
   })
-  .catch(error => {
-    error
-    window.console.error(error)
-    window.console.log('\nCollect ROWS ERROR')
-  })
 
-const fluxData = sensorValues;
+// window.console.log(sensorValues);
+
+const fluxData = sensorValues.map(x => x._value);
+window.console.log(fluxData);
 export {fluxData};
 
 new Vue({
